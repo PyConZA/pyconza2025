dbef4dbcb7e6f9e431c583370dab78b7a4f34f84
Sheena O'Connell <sheena.oconnell@gmail.com>
2025-05-09 14:30:55 +0200

    got markitup widget to look consistent

--- website/talks/forms.py
+++ website/talks/forms.py
@@ -3,6 +3,7 @@ from django import forms
 
 from wafer.talks.models import Talk
 from django_select2.forms import Select2MultipleWidget
+from markitup.widgets import MarkItUpWidget
 
 
 class TalkForm(wafer.talks.forms.TalkForm):
@@ -21,7 +22,12 @@ class TalkForm(wafer.talks.forms.TalkForm):
             "private_notes",
         )
         widgets = {
-            "abstract": forms.Textarea(attrs={"class": "input-xxlarge"}),
-            "notes": forms.Textarea(attrs={"class": "input-xxlarge"}),
+            # "abstract": forms.Textarea(attrs={"class": "input-xxlarge"}),
+            "abstract": MarkItUpWidget(
+                attrs={
+                    "class": "input-xxlarge textarea appearance-none border leading-normal block w-full border-gray-300 bg-white py-2 px-4 text-gray-700 rounded-lg focus:outline-none"
+                }
+            ),
+            "notes": forms.Textarea(),
             "authors": Select2MultipleWidget,
         }
