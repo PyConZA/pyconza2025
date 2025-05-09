951dda22f9167880d18925f3f51f2f58593c182c
Steve Yonkeu <yokwejuste@yahoo.com>
2025-05-10 00:46:09 +0100

    add debug toolbar and browser reload middleware; update settings and URLs

--- settings_dev.py
+++ settings_dev.py
@@ -1,5 +1,6 @@
-from settings_base import *
 from django.utils.translation import gettext_lazy as _
+
+from settings_base import *
 
 DEBUG = True
 
@@ -8,3 +9,13 @@ SECRET_KEY = "8iysa30^no&oi5kv$k1w)#gsxzrylr-h6%)loz71expnbf7z%)"
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = "gitignore/emails"
+
+
+def show_toolbar(request):
+    """Always show the debug toolbar"""
+    return True
+
+
+DEBUG_TOOLBAR_CONFIG = {
+    "SHOW_TOOLBAR_CALLBACK": show_toolbar
+}
