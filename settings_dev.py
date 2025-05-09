d219b5861438df26a45aaa859a0e49c871c884d1
Sheena O'Connell <sheena.oconnell@gmail.com>
2025-05-09 16:51:16 +0200

    cleaned up some unecessary changes

--- settings_dev.py
+++ settings_dev.py
@@ -8,6 +8,3 @@ SECRET_KEY = "8iysa30^no&oi5kv$k1w)#gsxzrylr-h6%)loz71expnbf7z%)"
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = "gitignore/emails"
-
-
-STATIC_URL = "static/"
