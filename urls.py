9e7c5ff097453d44c59d2286dddfd72021e3690f
Sheena O'Connell <sheena.oconnell@gmail.com>
2025-06-08 12:46:38 +0200

    password reset email now sends html and txt

--- urls.py
+++ urls.py
@@ -1,13 +1,24 @@
-from django.urls import include, re_path, path
+from django.urls import include, re_path, path, reverse_lazy
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.contrib.auth import views as auth_views
 
 
 admin.autodiscover()
 
+
 urlpatterns = [
+    path(
+        r"accounts/password/reset/",
+        auth_views.PasswordResetView.as_view(
+            success_url=reverse_lazy("auth_password_reset_done"),
+            html_email_template_name="registration/password_reset_email.html",
+            email_template_name="registration/password_reset_email.txt",
+        ),
+        name="auth_password_reset",
+    ),
     re_path(r"^accounts/", include("wafer.registration.urls")),
     re_path(r"^users/", include("wafer.users.urls")),
     re_path(r"^talks/", include("wafer.talks.urls")),
