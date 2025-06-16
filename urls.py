24cdd220a2daef2f756208bc6ac92e1ebe1b4e05
Sheena O'Connell <sheena.oconnell@gmail.com>
2025-06-16 13:06:20 +0200

    fix/grants url config

--- urls.py
+++ urls.py
@@ -29,7 +29,7 @@ urlpatterns = [
     re_path(r"^schedule/", include("wafer.schedule.urls")),
     re_path(r"^tickets/", include("wafer.tickets.urls")),
     re_path(r"^kv/", include("wafer.kv.urls")),
-    path(r"^opportunity_grants/", include("grants.urls")),
+    re_path(r"^opportunity_grants/", include("grants.urls")),
     path("__reload__/", include("django_browser_reload.urls")),
     path("", include("website.urls")),
 ]
