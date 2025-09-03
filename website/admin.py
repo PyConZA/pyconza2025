79a466920e97b7410df46ecbe1ec3c6000a56b63
Sheena O'Connell <sheena.oconnell@gmail.com>
2025-09-03 20:17:54 +0200

    improved user admin

--- website/admin.py
+++ website/admin.py
@@ -7,9 +7,25 @@ from django.utils.translation import gettext_lazy as _
 from wafer.users.admin import UserAdmin
 from wafer.talks.admin import TalkAdmin 
 from wafer.talks.models import Talk
+from grants.models import GrantApplication
+
+class TalkInline(admin.TabularInline):
+    model = Talk
+    extra = 0
+    fields = ('title', 'status', 'talk_type')
+    readonly_fields = ('title', 'status', 'talk_type')
+    can_delete = False
+
+class GrantApplicationInline(admin.StackedInline):
+    model = GrantApplication
+    extra = 0
+    fields = ('motivation', 'contribution', 'financial_need', 'request_travel', 'request_accommodation', 'request_ticket', 'created_at')
+    readonly_fields = ('motivation', 'contribution', 'financial_need', 'request_travel', 'request_accommodation', 'request_ticket', 'created_at')
+    can_delete = False
 
 class UserAdmin(UserAdmin, ImportExportModelAdmin):
-    pass
+    inlines = [TalkInline, GrantApplicationInline]
+
 admin.site.unregister(get_user_model())
 admin.site.register(get_user_model(), UserAdmin)
 
